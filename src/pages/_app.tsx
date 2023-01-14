5b915621f925faa122083da80b1bb9b2f53d05ef
aweijun <weijun.ang99@gmail.com>
2023-01-15 00:24:01 +0800

    Add Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@ import { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 import Layout from '@/components/layout';
 import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   const router = useRouter();
   const isLoginPath = router.pathname === '/login';
 
   return (
-    <Layout showHeader={!isLoginPath}>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout showHeader={!isLoginPath}>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   );
 }
