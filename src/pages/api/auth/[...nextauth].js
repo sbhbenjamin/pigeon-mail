a5501ede594409f03ff58c092fb7d80cb3c713d2
aweijun <weijun.ang99@gmail.com>
2023-01-15 00:40:26 +0800

    Authentication Into DB

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,17 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../server/utils/prisma"
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-
   ],
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
